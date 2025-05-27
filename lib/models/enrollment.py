7530e205b0462df6be4f059415f40badefbe3b71
Nick <nicknyler@gmail.com>
2025-05-27 12:35:08 +0300

    added enrollment.py code

---
+++ lib/models/enrollment.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from lib.database import Base
+from datetime import datetime
+
+class Enrollment(Base):
+    __tablename__ = "enrollments"
+    id = Column(Integer, primary_key=True)
+    course_id = Column(Integer, ForeignKey("courses.id"))
+    student_name = Column(String, nullable=False)
+    enrollment_date = Column(DateTime, default=datetime.utcnow)
+    course = relationship("Course")
